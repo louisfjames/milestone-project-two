f6f2677f35110c71b22adab666f88749e5a9ab66
louisfjames <louisfjames@hotmail.co.uk>
2026-02-19 12:56:45 +0000

    Build: Adding in search results div to bottom of main section and adding comments to script.js ready for code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Skiddle API
+
+// All wrapped in (document).ready(function) to ensure this code is run after the html document is full loadeed.
+
+$(document).ready(function () {
+
+  // All my code will go here.
+
+});
