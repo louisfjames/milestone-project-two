d680d41f510cbc1cfa9a93981040fa34040c710c
louisfjames <louisfjames@hotmail.co.uk>
2026-02-19 13:20:46 +0000

    Build: Adding API key const and DOM elements to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-// Skiddle API
+// -------------------------------------
+// Search bar code utilising Skiddle API 
+// -------------------------------------
+
+// jQuery is the code language of choice for script.js
 
 // All wrapped in (document).ready(function) to ensure this code is run after the html document is full loadeed.
-
 $(document).ready(function () {
 
-  // All my code will go here.
+    // API key stored as const as will need throughout code
+    const SKIDDLE_API_KEY = "481b24e5efaa9cbb9433c9119edda0d9";
+
+    // DOM Elements
+    const $searchBtn = $("#search-btn");
+    const $searchBox = $("#search-box");
+    const $resultsDiv = $("#results");
+    const $statusDiv = $("#status");
 
 });
